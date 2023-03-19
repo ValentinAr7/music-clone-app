a22e11030ee8c8140aec2d8bf417fafe4a71d4d9
Valentin <valio990210@gmail.com>
2023-03-19 15:06:49 +0200

    .

--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -2,10 +2,14 @@ import { Error, Loader, SongCard } from '../components';
 import { genres } from '../assets/constants';
 
 import { useGetTopChartsQuery } from '../redux/services/shazamCore';
+import { retry } from '@reduxjs/toolkit/dist/query';
 
 const Discover = () => {
     const {data, isFetching, error} = useGetTopChartsQuery();
     const genreTitle = 'Pop'
+
+    if(isFetching) return <Loader title='Loading songs...' />
+    if(error) return <Error/>
 
     return (
         <div className='flex flex-col'>
@@ -24,6 +28,16 @@ const Discover = () => {
                         {genres.map((genre) => <option key={genre.value} value={genre.value} >{genre.title} 
                         </option>)}
                 </select>   
+            </div>
+
+            <div className='flex flex-wrap sm:justify-start justify-center gap-8'>
+                {data?.map((song, i) =>(
+                <SongCard
+                key={song.key}
+                song={song}
+                i={i}
+                />
+                ))}
             </div>
         </div>
     )
