33d73b8472143425bd1ec518fce038f61417caf3
Valentin <valio990210@gmail.com>
2023-03-19 14:48:04 +0200

    .

--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -1,3 +1,5 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
 const options = {
     method: 'GET',
     headers: {
@@ -10,3 +12,19 @@ const options = {
     .then(response => response.json())
     .then(response => console.log(response))
     .catch(err => console.error(err));
+
+    export const shazamCoreApi = createApi({
+        reducerPath: 'shazamCoreApi',
+        baseQuery: fetchBaseQuery({
+            baseUrl: 'https://shazam-core.p.rapidapi.com/v1',
+           prepareHeaders: (headers) => {
+            headers.set('X-RapidAPI-Key', 'd784bbf8bemsh937dbecae345d21p133f8ejsne9677a6edd2d')
+
+            return headers;
+            }
+        })
+
+        endpoints: (builder) => ({
+            getTopCharts: builder.query({ query: '/charts/world'})
+        })
+    })
