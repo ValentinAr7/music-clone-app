aec5b82e050f6093d51f3c638717e8838b7228f5
Valentin <valio990210@gmail.com>
2023-03-19 16:29:29 +0200

    .

--- src/components/PlayPause.jsx
+++ src/components/PlayPause.jsx
@@ -1,5 +1,16 @@
-const PlayPause = () => (
-  <div>Loader</div>
+import { FaPauseCircle , FaPlayCirle } from 'react-icons/fa'
+
+
+{/*If current song is playing show pause circle
+   else show play circle */}
+const PlayPause = ({isPlaying, activeSong, song, handlePause, handlePlay}) => (
+  isPlaying && activeSong?.title === song.title ? (
+    
+    <FaPauseCircle/>
+  )  : (
+
+    <FaPlayCirle/>
+  )
 );
 
 export default PlayPause;
