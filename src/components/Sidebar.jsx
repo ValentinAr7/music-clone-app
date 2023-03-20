53216db30264d5cfa23d06e9f8ba052f746d63cb
Valentin <valio990210@gmail.com>
2023-03-20 20:04:07 +0200

    mobile version menu styling

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -33,18 +33,25 @@ const Sidebar = () => {
 
   return (
     <>
+      {/*Set styling for desctop menu */}
       <div className="md:flex hidden flex-col w-[240px] py-10 px-4 bg-[#191624]">
         <img src={logo} alt="logo" className="w-full h-14 object-contain" />
         <NavLinks />
       </div>
 
- {/*Set styling for mobile size */}
 
+      {/*Set styling for mobile size */}
       <div className="absolute md:hidden block top-6 right-3">
         {mobileMenuOpen ? (
-          <RiCloseLine />
-        ) : <HiOutlineMenu/>}
+          <RiCloseLine className="w-6 h-6 text-white mr-2" />
+        ) : <HiOutlineMenu className="w-6 h-6 text-white mr-2" />}
 
+      </div>
+
+      <div className={`absolute top-0 h-screen w-2/3 bg-gradient-to-tl from-white/10 to-[#483d8b] 
+      backdrop-blur-lg z-10 p-6 md:hidden smooth-transition ${mobileMenuOpen ? 'left-0' : '-left-full'}`}>
+        <img src={logo} alt="logo" className="w-full h-14 object-contain" />
+        <NavLinks />
       </div>
     </>
   )
