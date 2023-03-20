3cd22c2620c7864fd4a25345b71b231a13f9339c
Valentin <valio990210@gmail.com>
2023-03-20 19:52:11 +0200

    .

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -7,31 +7,36 @@ import { links } from "../assets/constants";
 
 //   {/*Map all links in the navbar */}
 const NavLinks = () => {
-   <div className="mt-10">
+  <div className="mt-10">
+
     {links.map((item) => (
-      <NavLink 
-      key={item.name}
-      to={item.to}
-      className='flex flex-row justify-start items-center my-8 
-      text-sm font-medium text-gray-400 hover:text-cyan-400'
-      onClick={() => handleClick && handleClick()}
+      <NavLink
+        key={item.name}
+        to={item.to}
+        className="flex flex-row justify-start items-center my-8 
+      text-sm font-medium text-gray-400 hover:text-cyan-400"
+        onClick={() => handleClick && handleClick()}
       >
-        <item.icon className="w-6 h-6 mr-2"/>
+        <item.icon className="w-6 h-6 mr-2" />
         {item.name}
       </NavLink>
     ))}
-   </div>
+  </div>
 }
 
 const Sidebar = () => {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
 
-  return(
-    <div className="md:flex hidden flex-col w-[240px] py-10 px-4 bg-[#191624]">
-      <img src={logo} alt="logo" className="w-full h-14 object-contain" />
-      <NavLinks/>
-    </div>
+  return (
+    <>
+      <div className="md:flex hidden flex-col w-[240px] py-10 px-4 bg-[#191624]">
+        <img src={logo} alt="logo" className="w-full h-14 object-contain" />
+        <NavLinks />
+      </div>
+
+      <div></div>
+    </>
   )
 }
-  
+
 export default Sidebar;
