3ed90ba99eb839b1e20d78d6cf68ea1a10897e91
Valentin <valio990210@gmail.com>
2023-03-23 13:57:25 +0200

    fetch song details

--- src/pages/SongDetails.jsx
+++ src/pages/SongDetails.jsx
@@ -1,3 +1,23 @@
-const SongDetails = () => <div>SongDetails</div>;
+import { useParams } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import { DetailsHeader, Error, Loader, RelatedSongs } from "../components";
+import { useGetSongDetailsQuery } from "../redux/services/shazamCore";
+import { setActiveSong, playPause } from "../redux/features/playerSlice";
+
+const SongDetails = () => {
+
+    const dispatch = useDispatch();
+    const { songid } = useParams();
+    const { activeSong, isPlaying } = useSelector((state) => state.player);
+    const { data: songData, isFetching: isFetchingSongDetails } =
+    useGetSongDetailsQuery({songid})
+    console.log(songid);
+
+    return(
+        <div className="flex flex-col">
+            <DetailsHeader artistId={artistId} songData={songData}/>
+        </div>
+    )
+}
 
 export default SongDetails;
