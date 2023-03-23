152fc9135244daa6f862c9cedc15d127b3edc16b
Valentin <valio990210@gmail.com>
2023-03-23 14:08:50 +0200

    display lyrics

--- src/pages/SongDetails.jsx
+++ src/pages/SongDetails.jsx
@@ -15,7 +15,21 @@ const SongDetails = () => {
 
     return(
         <div className="flex flex-col">
-            <DetailsHeader artistId={artistId} songData={songData}/>
+            {/* <DetailsHeader artistId={artistId} songData={songData}/> */}
+
+            <div className="mb-10">
+                <h2 className="text-white text-3x1 font-bold">
+                    Lyrics:
+                </h2>
+
+                {/* If lyrics exist map over them else display sorry message */}
+                <div className="mt-5">
+                    {songData?.sections[1].type === 'LYRICS'
+                    ? songData?.sections[1].text.map((line, i) =>(
+                        <p className="text-gray-400 text-base my-1">{line}</p>
+                    )) : <p className="text-gray-400 text-base my-1">Sorry, no lyrics found!</p> }
+                </div>
+            </div>
         </div>
     )
 }
--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -12,9 +12,10 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
             }
         }),
         endpoints: (builder) => ({
-            getTopCharts: builder.query({ query: () => '/charts/world'})
+            getTopCharts: builder.query({ query: () => '/charts/world'}),
+
+        getSongDetails: builder.query({ query: ({songid}) => `/tracks/details?track_id=${songid}`}),
         }),
-        getSongDetails: builder.query({query: ({songid}) => `/tracks/details?track_id=${songid}`})
     });
 
     export const {
